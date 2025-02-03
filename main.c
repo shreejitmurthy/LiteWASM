eb0704cd7b29482b0109ccf0ea5ffe2379222b05
Shreejit Murthy <botmorph17@gmail.com>
2025-02-03 22:05:52 +1030

    read into buffer

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define WASM_HEADER_SIZE 8
 /* 
- * Every .wasm file begins with the same 8 codes:
- * 00 61 73 6D 01 00 00 00 
+ * Every .wasm file begins with the magic number and the version number:
+ * Magic:   00 61 73 6D
+ * Version: 01 00 00 00 
+ * NOTE: The version of the WebAssembly binary format may increase in the future.
+ * We'll just check for both numbers.
  */
 const uint8_t valid_header[WASM_HEADER_SIZE] = { 0x00, 0x61, 0x73, 0x6D, 0x01, 0x00, 0x00, 0x00 };
 
@@ -15,20 +19,23 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    FILE* file = fopen(argv[0], "rb");
-    int valid_wasm = 0;
-    // We'll check the header to ensure the .wasm file is valid
+    FILE* file = fopen(argv[1], "rb");
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // First check the header to ensure the .wasm file is valid
     uint8_t header[WASM_HEADER_SIZE];
     size_t bytesRead = fread(header, 1, WASM_HEADER_SIZE, file);
 
     if (bytesRead < WASM_HEADER_SIZE) {
         printf("File is too small to be valid WASM.");
-        valid_wasm = 0;
         return 1;
     }
 
     if (memcmp(header, valid_header, WASM_HEADER_SIZE) == 0) {
-        valid_wasm = 1;
+
     } else {
         printf("Invalid .wasm file header!\nExpected:");
         for (int i = 0; i < WASM_HEADER_SIZE; i++)
@@ -40,6 +47,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // Now read entire file into a buffer
+    fseek(file, 0, SEEK_END); 
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET); 
+    uint8_t* buffer = (uint8_t*)malloc(size + 1);
+
+    fread(buffer, size, 1, file);
+
+    size_t cursor = 0;
+    
+    free(buffer);
 
     return 0;
 }
